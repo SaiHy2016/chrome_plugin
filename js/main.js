517f5ef1600af77bfc55c23cfc9d1af306acb962
hy <huyuan2016@yeah.net>
2018-01-15 08:47:30 +0800

    chrome 插件

---
+++ js/main.js
@@ -0,0 +1,55 @@
+var cs=function () {
+    console.log(...arguments)
+}
+
+function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+    var pc = new myPeerConnection({iceServers: []}), // 空的ICE服务器（STUN或者TURN）
+        noop = function() {},
+        localIPs = {}, //记录有没有被调用到onNewIP这个listener上
+        ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+        key;
+
+    function ipIterate(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+    pc.createDataChannel(""); //create a bogus data channel
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(ipIterate);
+        });
+        pc.setLocalDescription(sdp, noop, noop);
+    }); // create offer and set local description
+    pc.onicecandidate = function(ice) { //listen for candidate events
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+    };
+}
+
+// Vue.config.devtools = true;
+
+new Vue({
+    el:'#app',
+    data:{
+        ip:'',
+        url:''
+    },
+    template:`<div>本地ip地址:{{ip}}<br>{{url}}
+                    <div ref="qr" style="text-align: center"></div>
+                </div>`,
+    mounted:function () {
+        findIP(ip=>{
+            this.ip=ip;
+            chrome.tabs.query({
+                active: true
+            }, function(tabArray){
+                console.log(tabArray[0].url)
+                this.url=tabArray[0].url.replace(/:\/\/[^\/]*/,'://'+this.ip);
+                $(this.$refs.qr).qrcode({text:this.url,width:100,height:100})
+            }.bind(this));
+        });
+
+    }
+})
